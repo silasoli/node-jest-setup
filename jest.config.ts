214cc0b5b6cc6398cf6547604e7d1d50268f6964
silasoli <silasoliv39@gmail.com>
2025-02-02 19:28:13 -0300

    feat: configure watch mode

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,8 @@ import type { Config } from  "@jest/types";
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    verbose: true
+    verbose: true,
+    watch: true
 }
 
 export default config;
