212694c9bb61414120afefc8de4886f7fa09d6c6
silasoli <silasoliv39@gmail.com>
2025-10-19 21:43:54 -0300

    chore: delete base branch code

--- node/src/test/server_app/data/IdGeneretor.test.ts
+++
@@ -1,9 +0,0 @@
-import { generateRandomId } from "../../../app/server_app/data/IdGenerator";
-
-describe('IdGeneretor test suite', () => {
-    it('should return id', () => {
-        const actual = generateRandomId()
-        expect(actual).toBeDefined();
-        expect(actual.length).toBe(20);
-    })
-})
--- react/ts-test-course/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      App works!
-    </div>
-  );
-}
-
-export default App;
