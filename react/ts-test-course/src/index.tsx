f5d4f56093f0a0c47942e447df87982d61b729de
silasoli <silasoliv39@gmail.com>
2025-10-19 21:36:34 -0300

    feat: react testing

--- react/ts-test-course/src/index.tsx
+++ react/ts-test-course/src/index.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import LoginComponent from './LoginComponent';
+import LoginService from './services/LoginService';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const loginService = new LoginService();
+
+const setToken = (token: string) =>{console.log('RECEIVED TOKEN', token)}
+
+root.render(
+    <LoginComponent loginService={loginService} setToken={setToken}/>
+);
